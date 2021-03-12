e5e7533c1ac986dfd52d7a80de219afb5816148c
steph <sbullier@gmail.com>
2021-03-12 16:37:06 +0100

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Erreur 404</h2>
+            <p>Cette page n'existe pas.</p>
+        </div>
+    )
+}
