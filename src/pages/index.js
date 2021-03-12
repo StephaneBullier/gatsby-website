d7a80bf73a397bd10ebb792749428ac4dfff1b7e
steph <sbullier@gmail.com>
2021-03-12 17:43:21 +0100

    Add module css styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react"
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
+import * as styles from '../styles/home.module.css'
+import {Link} from 'gatsby';
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
+          <h3>Develop & deploy</h3>
+          <p>Web developer based in Montpellier.</p>
+          <Link className={styles.btn} to="/projets">Mon portfolio</Link>
         </div>
       </section>
     </Layout>
--- src/pages/projets/index.js
+++ src/pages/projets/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout';
+import * as styles from '../../styles/projets.module.css'
 
 export default function Projets() {
     return (
       <Layout>
-        <div>
+        <div className={styles.portfolio}>
           <h2>Mes projets</h2>
+          <h3>Projets et sites web</h3>
         </div>
       </Layout>
     )
