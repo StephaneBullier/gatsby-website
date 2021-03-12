fb40a416097b13ad4e6e8bbb24a74b351a22620d
steph <sbullier@gmail.com>
2021-03-12 16:37:16 +0100

    Add about page

---
+++ src/pages/a-propos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function aPropos() {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <p>Ipsum do ut ipsum nisi consequat occaecat Lorem.</p>
+            <p>Ipsum do ut ipsum nisi consequat occaecat Lorem.</p>
+            <p>Ipsum do ut ipsum nisi consequat occaecat Lorem.</p>
+        </div>
+    )
+}
