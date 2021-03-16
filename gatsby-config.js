b2a521f81748f821965146d55e50166d594ed8a7
steph <sbullier@gmail.com>
2021-03-16 11:46:30 +0100

    Add plugins to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `project`,
+        path: `${__dirname}/src/projects/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    }
+  ],
   siteMetadata: {
     title: 'Web Warrior',
     description: 'Portfolio d\'un développeur web',
+    email: 'contact@thewebwarrior.com'
   }
 }
